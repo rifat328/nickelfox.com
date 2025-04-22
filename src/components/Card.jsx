9d3ae9728f1cc173ff3f5cbb3333d97b3e86d08b
Rifat Hossain <69417553+rifat328@users.noreply.github.com>
2025-04-23 04:19:17 +0600

    feat: add react-icons dependency and update Card component link

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-
+import { FaArrowRightLong } from "react-icons/fa6";
 const Card = (props) => {
   return (
     <div>
       <img src={props.src} alt={props.title} className="card-img" />
       <h2 className="card-title">{props.title}</h2>
       <h3 className="card-description">{props.description}</h3>
-      <a href={props.link} className="card-title" />
+      <a href={props.link} className="card-link">
+        <FaArrowRightLong />
+      </a>
     </div>
   );
 };
