cbc9bcbf1ddf7630030e16d003172733cdadcfb5
Rifat Hossain <69417553+rifat328@users.noreply.github.com>
2025-04-26 00:37:42 +0600

    refactor: restructure Card component layout for improved readability

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import { FaArrowRightLong } from "react-icons/fa6";
 const Card = (props) => {
   return (
-    <div>
+    <div className="card">
       <img src={props.src} alt={props.title} className="card-img" />
-      <h2 className="card-title">{props.title}</h2>
-      <h3 className="card-description">{props.description}</h3>
-      <a href={props.link} className="card-link">
-        <FaArrowRightLong />
-      </a>
+      <div class="card-detail-container">
+        <h2 className="card-title">{props.title}</h2>
+        <h3 className="card-description">{props.description}</h3>
+        <a href={props.link} className="card-link">
+          <FaArrowRightLong />
+        </a>
+      </div>
     </div>
   );
 };
